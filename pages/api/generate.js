08b156ca42bc6e7593839bc672f4f5d3d30de212
seaweedlover <jiannanwang913@gmail.com>
2023-05-20 16:53:20 -0700

    Set up basic grief support chatbot

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,6 +5,10 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const systemMsg = `You are an assistant to provide grief and emotional support to users who have lost their families or friends. 
+In the following conversation, you should speak like users' mom in a caring, loving, and natural way. 
+Please don't mention you are a language model or AI in any of your responses.`;
+
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -15,8 +19,8 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const userMsg = req.body.userMessage || '';
+  if (userMsg.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
@@ -25,38 +29,46 @@ export default async function (req, res) {
     return;
   }
 
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
+  let context = [{'role': 'system', content: systemMsg}];
+
+  await collectMessages();
+  console.log('context: ', context);
+
+  async function getCompletionFromMessages(messages) {
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        stream: true,
+        temperature: 0.6,
       });
+      console.log('result: ', JSON.stringify(completion.data.choices[0].message));
+      const result = completion.data.choices[0].message.content;
+      res.status(200).json({ result: result });
+
+    } catch(error) {
+      // Consider adjusting the error handling logic for your use case
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+          error: {
+            message: 'An error occurred during your request.',
+          }
+        });
+      }
     }
+
+    return result;
   }
-}
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  async function collectMessages()  {
+    var prompt = userMsg;
+    context.push({ role: 'user', content: prompt});
+    var response = await getCompletionFromMessages(context);
+    context.push({ role: 'assistant', content: response});
+  
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [userInput, setUserInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ userMessage: userInput }),
       });
 
       const data = await response.json();
@@ -23,7 +23,6 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -34,23 +33,24 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Grief Support Chatbot</title>
+        <link rel="icon" href="./hug-icon.webp" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="./hug-icon.webp" className={styles.icon} />
+        <h3>Grief Support Chatbot</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="userMsg"
+            placeholder="Enter what you want to say here"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Submit" />
         </form>
+        <div className={styles.input}>{userInput}</div>
         <div className={styles.result}>{result}</div>
       </main>
     </div>
